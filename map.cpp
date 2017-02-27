e94aa886230d31ed1344d70dd885fb8f466f937f
PaulSkogen2 <paul.skogen2@gmail.com>
2017-02-27 10:41:24 -0600

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int main()
 {
-    cout << "***************************************************************" << endl
-         << "*Level |Thorny|______|Giant | ______|Flatulent|_____#Treasure#*" << endl
-         << "*  1   | Bush |      |Spider|       |  Dwarf  |     #Palace 1#*" << endl
-         << "*         |              |               |               |    *" << endl
-         << "*         v              v               v               |    *" << endl
-         << "*     (Treasure)     (Treasure)      (Treasure)          |    *" << endl
-         << "*     (Room 1A)      (Room 1B)       (Room 1C)           |    *" << endl
-         << "*   _____________________________________________________|    *" << endl
-         << "*  |                                                          *" << endl
-         << "*  v                                                          *" << endl
-         << "*Level |Raging|______ |Angry| ______|Cave |_________#Treasure#*" << endl
-         << "*  2  |Minotaur|     |Old Lady|     |Troll|         #Palace 2#*" << endl
-         << "*         |              |             |                 |    *" << endl
-         << "*         v              v             v                 |    *" << endl
-         << "*     (Treasure)     (Treasure)    (Treasure)            |    *" << endl
-         << "*     (Room 2A)      (Room 2B)     (Room 2C)             |    *" << endl
-         << "*   _____________________________________________________|    *" << endl
-         << "*  |                                                          *" << endl
-         << "*  v                                                          *" << endl
-         << "*Level |Master|______|Enraged| _____|Flaming|________!!!YOU!!!*" << endl
-         << "*  3   |Vampire|     |Assassin|     |Dragon|         !!!WIN!!!*" << endl
-         << "*         |              |                                    *" << endl
-         << "*         v              v                                    *" << endl
-         << "*     (Treasure)     (Treasure)                               *" << endl
-         << "*     (Room 2A)      (Room 2B)                                *" << endl
-         << "***************************************************************" << endl;
+    cout << "*****************************************************************" << endl
+         << "* Level |Thorny|______|Giant | ______|Flatulent|_____#Treasure# *" << endl
+         << "*   1   | Bush |      |Spider|       |  Dwarf  |     #Palace 1# *" << endl
+         << "*          |              |               |               |     *" << endl
+         << "*          v              v               v               |     *" << endl
+         << "*      (Treasure)     (Treasure)      (Treasure)          |     *" << endl
+         << "*      (Room 1A)      (Room 1B)       (Room 1C)           |     *" << endl
+         << "*    _____________________________________________________|     *" << endl
+         << "*   |                                                           *" << endl
+         << "*   v                                                           *" << endl
+         << "* Level |Raging|______ |Angry| ______|Cave |_________#Treasure# *" << endl
+         << "*   2  |Minotaur|     |Old Lady|     |Troll|         #Palace 2# *" << endl
+         << "*          |              |             |                 |     *" << endl
+         << "*          v              v             v                 |     *" << endl
+         << "*      (Treasure)     (Treasure)    (Treasure)            |     *" << endl
+         << "*      (Room 2A)      (Room 2B)     (Room 2C)             |     *" << endl
+         << "*    _____________________________________________________|     *" << endl
+         << "*   |                                                           *" << endl
+         << "*   v                                                           *" << endl
+         << "* Level |Master|______|Enraged| _____|Flaming|________!!!YOU!!! *" << endl
+         << "*   3   |Vampire|     |Assassin|     |Dragon|         !!!WIN!!! *" << endl
+         << "*          |              |                                     *" << endl
+         << "*          v              v                                     *" << endl
+         << "*      (Treasure)     (Treasure)                                *" << endl
+         << "*      (Room 2A)      (Room 2B)                                 *" << endl
+         << "*****************************************************************" << endl;
     return 0;
 }
