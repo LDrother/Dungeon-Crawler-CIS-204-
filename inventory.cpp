6be71f9eddacf412c86196a06a8f6d9c7725fcea
Gstrong3 <gmstrong1@umary.edu>
2017-03-10 12:18:53 -0600

    Add files via upload

---
+++ inventory.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int Lvl1armour
+
+int Lvl11armour
+int Lvl12armour
+int Lvl13armour
+
+int Lvl2armour
+
+int Lvl21armour
+int Lvl22armour
+int Lvl23armour
+
+int Lvl3armour
+
+int Lvl31armour
+int Lvl32armour
+
+
+int main()
+{
+ switch(armour)
+ {
+ case 'ws':
+
+ }
+    return 0;
+}
