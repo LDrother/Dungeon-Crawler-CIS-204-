6aae8407176ea5b515b386c2b9e8cc49fe8fda57
Ldr <rotherlincoln@hotmail.com>
2017-03-11 17:33:58 -0600

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -2,25 +2,8 @@
 
 using namespace std;
 
-int Lvl1armour
 
-int Lvl11armour
-int Lvl12armour
-int Lvl13armour
-
-int Lvl2armour
-
-int Lvl21armour
-int Lvl22armour
-int Lvl23armour
-
-int Lvl3armour
-
-int Lvl31armour
-int Lvl32armour
-
-
-int main()
+void inventory::inventory()
 {
  switch(armour)
  {
@@ -73,5 +56,4 @@ int main()
 
 
  }
-    return 0;
 }
