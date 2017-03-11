40725be5e449a0e1161493714990cfaf196651da
Ldr <rotherlincoln@hotmail.com>
2017-03-11 16:54:57 -0600

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -30,5 +30,4 @@ void Map::Map()
          << "*      (Treasure)     (Treasure)                                *" << endl
          << "*      (Room 2A)      (Room 2B)                                 *" << endl
          << "*****************************************************************" << endl;
-    return 0;
 }
