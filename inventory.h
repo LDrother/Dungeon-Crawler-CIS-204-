7f9d259643e4e845f4f2265eede5a508957aef77
Ldr <rotherlincoln@hotmail.com>
2017-03-13 11:10:01 -0500

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -6,17 +6,7 @@ class Map
 {
     public:
         void inventory();
-        int Lvl1armour
-        int Lvl11armour
-        int Lvl12armour
-        int Lvl13armour
-        int Lvl2armour
-        int Lvl21armour
-        int Lvl22armour
-        int Lvl23armour
-        int Lvl3armour
-        int Lvl31armour
-        int Lvl32armour
+        int armour;
 
     protected:
 
