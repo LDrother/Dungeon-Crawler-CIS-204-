5a842dd337b1e49120a01aefedab1da6f1bcc1b8
Gstrong3 <gmstrong1@umary.edu>
2017-03-13 12:06:13 -0500

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -31,58 +31,84 @@ void inventory::inventory()
  
  
  armourMenu1:
-  case 10:
-  cout << "you are wearing simple sandals, a plain cloak, and carrying a walking stick" << endl;
-  break;
+  
+  cout << " you are wearing simple sandals, a plain cloak, and carrying a walking stick" << endl;
  
-  case 11:
- 	cout << "you are now equipped with some comfy leather boots, some worn leather armour, and you've found a rusty knife" << endl;
- 	break;
  
-  case 12:
- 	cout << "you are now wearing flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that..." << endl;
- 	break;
+ 
+ 	cout << "comfy leather boots, some worn leather armour, and you've found a rusty knife-- To select this armour press 11+enter" << endl;
+ 
+  
+ 	cout << "flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that-- to select this armour press 12+enter" << endl;
+ 
 
   case 13:
- 	cout << "you are now wearing steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan" << endl;
- 	break;
-
+ 	cout << "steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan--to select this armour press 13+enter" << endl;
+ 
 armourMenu2:
  {
-  case 10:
-  cout << "you are wearing simple sandals, a plain cloak, and carrying a walking stick" << endl;
-  break;
+  
+  cout << "simple sandals, a plain cloak, and carrying a walking stick--to select this armour press 10+enter" << endl;
+  
  
-  case 11:
- 	cout << "you are now equipped with some comfy leather boots, some worn leather armour, and you've found a rusty knife" << endl;
- 	break;
  
-  case 12:
- 	cout << "you are now wearing flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that..." << endl;
- 	break;
-
-  case 13:
- 	cout << "you are now wearing steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan" << endl;
- 	break;
-
-  case 2:
- 	cout << " to start out level two, you have combat boots, celtic warriors skirt, and a sword with a poisonous blade" << endl;
- 	break;
+ 	cout << "comfy leather boots, some worn leather armour, and you've found a rusty knife--to select this armour press 11+enter" << endl;
  
-  case 21:
- 	cout << " you are now wearing some tennis shoes, shorts and a Tshirt, and a packing 50cal handgun" << endl;
- 	break;
+ 
 
-  case 22:
- 	cout << " you have decided to put on high heels, a pink sundress, and are now carrying a handbag with garlic in it" endl;
- 	break;
+ 	cout << "flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that--to select this armour press 12+enter" << endl;
 
-  case 23:
- 	cout << " you are now bare foot, wearing a loin cloth, and brandishing the american flag and an RPG" << endl;
- 	break;
+
+ 	cout << "steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan--to select this armour press 13+enter" << endl;
+ 
+ 
+ 	cout << "combat boots, celtic warriors skirt, and a sword with a poisonous blade--to select this armour press 20+enter" << endl;
+ 	
+
+ 	cout << "tennis shoes, shorts and a Tshirt, and a packing 50cal handgun--to select this armour press 21+enter" << endl;
+ 	
+  
+ 	cout << "high heels, a pink sundress, and are now carrying a handbag with garlic in it--to select this armour press 22+enter" endl;
+   
+ 	cout << " bare foot, wearing a loin cloth, and brandishing the american flag and an RPG--to select this armour press 23+enter" << endl;
  
 
 armourMenu3:
+   {
+  
+  cout << "simple sandals, a plain cloak, and carrying a walking stick--to select this armour press 10+enter" << endl;
+  
+ 
+ 
+ 	cout << "comfy leather boots, some worn leather armour, and you've found a rusty knife--to select this armour press 11+enter" << endl;
+ 
+ 
+
+ 	cout << "flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that--to select this armour press 12+enter" << endl;
+
+
+ 	cout << "steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan--to select this armour press 13+enter" << endl;
+ 
+ 
+ 	cout << "combat boots, celtic warriors skirt, and a sword with a poisonous blade--to select this armour press 20+enter" << endl;
+ 	
+
+ 	cout << "tennis shoes, shorts and a Tshirt, and a packing 50cal handgun--to select this armour press 21+enter" << endl;
+ 	
+  
+ 	cout << "high heels, a pink sundress, and are now carrying a handbag with garlic in it--to select this armour press 22+enter" endl;
+   
+ 	cout << " bare foot, wearing a loin cloth, and brandishing the american flag and an RPG--to select this armour press 23+enter" << endl;
+ 
+   
+ 	cout << "steel armoured boots, titanium armour, and your dad's hack-saw--to select this armour press 30+enter" << endl;
+ 
+ 	cout << " become a beautiful long blonde highschool girl, carrying a pumkin spiced latte, and a bad attitude--to selec this.. 'option' press 31+enter" << endl;
+ 
+ 	cout << " gold boots, fire repelent shirt, and magic mace... hint: go for the eyes-- to select this option press 32+enter" << endl;
+ 
+
+
 {
  switch(armour)
  {
