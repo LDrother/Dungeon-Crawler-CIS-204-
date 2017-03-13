92b6cd7cb968fb5d9de57d59dc64349537e3b85e
Gstrong3 <gmstrong1@umary.edu>
2017-03-13 11:50:04 -0500

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -3,7 +3,86 @@
 using namespace std;
 
 
+ {
+        if(choice == 3)
+        {
+         cout << "you have selected to change your armour" << endl;
+        }
+        if (1A != Complete)
+        {
+         cout armourMenu1;
+        }
+        if (1C == Complete)
+        {
+         cout armourMenu2;
+        }
+        if (2C == Complete)
+        {
+         cout armourMenu3;
+        }
+        
+    }
+
+
 void inventory::inventory()
+ 
+ int Main()
+ 
+ 
+ 
+ armourMenu1:
+  case 10:
+  cout << "you are wearing simple sandals, a plain cloak, and carrying a walking stick" << endl;
+  break;
+ 
+  case 11:
+ 	cout << "you are now equipped with some comfy leather boots, some worn leather armour, and you've found a rusty knife" << endl;
+ 	break;
+ 
+  case 12:
+ 	cout << "you are now wearing flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that..." << endl;
+ 	break;
+
+  case 13:
+ 	cout << "you are now wearing steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan" << endl;
+ 	break;
+
+armourMenu2:
+ {
+  case 10:
+  cout << "you are wearing simple sandals, a plain cloak, and carrying a walking stick" << endl;
+  break;
+ 
+  case 11:
+ 	cout << "you are now equipped with some comfy leather boots, some worn leather armour, and you've found a rusty knife" << endl;
+ 	break;
+ 
+  case 12:
+ 	cout << "you are now wearing flip-flops, a swim-suit, and carrying a giant Pool Noodle... not sure why you'd go with that..." << endl;
+ 	break;
+
+  case 13:
+ 	cout << "you are now wearing steel tipped work boots, a nice chainmail shirt, and you are armed with your mother's frying pan" << endl;
+ 	break;
+
+  case 2:
+ 	cout << " to start out level two, you have combat boots, celtic warriors skirt, and a sword with a poisonous blade" << endl;
+ 	break;
+ 
+  case 21:
+ 	cout << " you are now wearing some tennis shoes, shorts and a Tshirt, and a packing 50cal handgun" << endl;
+ 	break;
+
+  case 22:
+ 	cout << " you have decided to put on high heels, a pink sundress, and are now carrying a handbag with garlic in it" endl;
+ 	break;
+
+  case 23:
+ 	cout << " you are now bare foot, wearing a loin cloth, and brandishing the american flag and an RPG" << endl;
+ 	break;
+ 
+
+armourMenu3:
 {
  switch(armour)
  {
