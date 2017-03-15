3449ed21524353fbd2f9a1f6cf3ba2f74eca478f
Ldr <rotherlincoln@hotmail.com>
2017-03-15 12:14:20 -0500

    Update Global.h

--- Global.h
+++ Global.h
@@ -5,7 +5,7 @@ class Global
 {
 
 public:
-    extern int room_num = 0, Player_HP, Bush_HP, Spider_HP, Dwarf_HP, Minotaur_HP, Lady_HP,
+    extern int Rm_Num, Player_HP, Bush_HP, Spider_HP, Dwarf_HP, Minotaur_HP, Lady_HP,
     Troll_HP, Vampire_HP, Assassin_HP, Dragon_HP;
     
     void Global();
