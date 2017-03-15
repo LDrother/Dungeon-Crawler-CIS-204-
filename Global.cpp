96fd47225164e479d01453c266e32c7c5c9b8058
Ldr <rotherlincoln@hotmail.com>
2017-03-15 12:16:19 -0500

    Update Global.cpp

--- Global.cpp
+++ Global.cpp
@@ -3,7 +3,17 @@
 
 using namespace std;
 
-Global::Global()
+void Global::Global()
 {
-
+  int Rm_Num = 0;
+  int Player_HP = 10;
+	int Bush_HP = 3;
+	int SPider_HP = 10;
+	int Dwarf_HP = 15;
+	int Minotaur_HP = 30;
+	int Lady_HP = 25;
+	int Troll_HP = 40;
+	int Vampire_HP = 35;
+	int Assassin_HP = 30;
+	int Dragon_HP = 300;
 }
