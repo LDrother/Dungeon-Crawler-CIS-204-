cce13eb6b49576855a8791ddea4805d66c508388
PaulSkogen2 <paul.skogen2@gmail.com>
2017-04-07 11:26:16 -0500

    Update Global.h

--- Global.h
+++ Global.h
@@ -1,14 +1,23 @@
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
+#include <string>
+
+using namespace std;
 
 class Global
 {
 
 public:
-    extern int Rm_Num, Equipment, FirstAttacker, MonsterAttack, PlayerAttack, Player_HP, Bush_HP, Spider_HP, Dwarf_HP, Minotaur_HP, Lady_HP,
-    Troll_HP, Vampire_HP, Assassin_HP, Dragon_HP;
-    
-    void Global();
+
+    int Player_HP = 10 , Bush_HP = 3, Spider_HP = 10, Dwarf_HP = 15, Minotaur_HP, Lady_HP,
+    Troll_HP, Vampire_HP, Assassin_HP, Dragon_HP, Rm_Num = 0, choice = 0;
+
+    Global();
+    menu();
+
+
+
 };
 
 #endif
+
