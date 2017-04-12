1319f9ca2ab47cb421e9c7b5870d51d2116030fc
PaulSkogen2 <paul.skogen2@gmail.com>
2017-04-12 12:28:57 -0500

    Update Global.h

--- Global.h
+++ Global.h
@@ -9,8 +9,8 @@ class Global
 
 public:
 
-    int Player_HP = 10 , Bush_HP = 3, Spider_HP = 10, Dwarf_HP = 15, Minotaur_HP, Lady_HP,
-    Troll_HP, Vampire_HP, Assassin_HP, Dragon_HP, Rm_Num = 0, choice = 0;
+    int Player_HP = 10 , Bush_HP = 3, Spider_HP = 10, Dwarf_HP = 15, Minotaur_HP = 30, Lady_HP = 25,
+    Troll_HP = 40, Vampire_HP = 35, Assassin_HP = 30, Dragon_HP = 300, Rm_Num = 0, choice = 0, armour = 0;
 
     Global();
     menu();
@@ -20,4 +20,3 @@ public:
 };
 
 #endif
-
